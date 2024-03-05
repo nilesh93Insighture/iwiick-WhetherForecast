9c824e4848f70c5e7829ec206be8198ae0967fa6
iwiick <heshan.wickramaratne@insighture.com>
2024-03-05 09:09:41 +0530

    .env added

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const https = require("https");
+const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
+
+app.use(cors());
+
 app.listen(3001, function () {
+  if (process.env.API_KEY) {
+    console.log(`'API_KEY' loaded`);
+  } else {
+    console.log(`'API_KEY' is needed in the .env file`);
+  }
   console.log("app running on port 3001");
 });
+
 app.get("/", function (req, res) {
   const query = "Sri Lanka";
-  const apikey = "838ff09b2e2762710a8513b183a16a60";
+  const apikey = process.env.API_KEY;
   const unit = "metric";
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
